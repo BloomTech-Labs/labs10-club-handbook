c471c5d38d7fc65d1331bd2deabd6de303a227ca
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-13 15:08:56 -0500

    updated users seed to seed 501 users, seeded devDB with this

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,22 +1,41 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex, Promise) {
+  //create arrays to insert
+  let users1 = [];
+  let users2 = [];
+  let users3 = [];
+  let seedLimit = 167;
+  for (let i = 1; i <= seedLimit; i++) {
+    users1.push({
+      username: `TestUser${i}`,
+      email: `user${i}@test.fake`,
+      password: bcrypt.hashSync("pass", 12)
+    });
+    users2.push({
+      username: `TestUser${i + seedLimit}`,
+      email: `user${i + seedLimit}@test.fake`,
+      password: bcrypt.hashSync("pass", 12)
+    });
+    users3.push({
+      username: `TestUser${i + 2 * seedLimit}`,
+      email: `user${i + 2 * seedLimit}@test.fake`,
+      password: bcrypt.hashSync("pass", 12)
+    });
+  }
   // Deletes ALL existing entries
   return knex("users")
     .truncate()
     .then(function() {
       // Inserts seed entries
-      return knex("users").insert([
-        {
-          username: "TestUser1",
-          password: bcrypt.hashSync("pass", 12),
-          email: "fake@fake.fake"
-        },
-        {
-          username: "TestUser2",
-          password: bcrypt.hashSync("pass", 12),
-          email: "test@fake.fake"
-        }
-      ]);
+      return knex("users")
+        .insert(users1)
+        .then(function() {
+          return knex("users")
+            .insert(users2)
+            .then(function() {
+              return knex("users").insert(users3);
+            });
+        });
     });
 };
