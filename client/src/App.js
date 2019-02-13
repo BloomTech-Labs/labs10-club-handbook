976778af5dd2c60ba87c0fccdb49b09b730b1c93
darenlarson <larson.daren@gmail.com>
2019-02-13 17:42:08 -0600

    Auth0 config

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import Payment from './billing/billing';
+import Login from './auth/Login';
+import Callback from './auth/Callback';
+import Authenticated from './auth/Authenticated';
 
 class App extends Component {
    render() {
@@ -10,6 +13,9 @@ class App extends Component {
          <BrowserRouter>
             <div className="App">
                <Switch>
+                  <Route path="/login" component={Login} />
+                  <Route path="/callback" component={Callback} />
+                  <Route path="/authenticated" render={props => <Authenticated {...props} />} />
                   <Route exact path="/" component={Dashboard} />
                   <Route exact path="/billing" component={Payment} />
                </Switch>
