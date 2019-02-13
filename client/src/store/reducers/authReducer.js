86c6650afa08f37eff6c9a795776857e5e6b2693
ToMMaaY <thuynh629@gmail.com>
2019-02-13 12:18:48 -0800

    setup the auth reducer

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,7 +1,56 @@
 import { START, FAIL, LOGIN, REGISTER, LOGOUT } from '../actions/authActions';
 
-initialState = {};
+initialState = {
+   currentUser: null,
+   loading: false,
+   message: null,
+   error: null,
+};
 
-const authReducer = (state = initialState, action) => {};
+const authReducer = (state = initialState, action) => {
+   switch (action.type) {
+      case START:
+         return {
+            ...state,
+            loading: true,
+            message: action.message,
+         };
+
+      case FAIL:
+         return {
+            ...state,
+            loading: false,
+            message: null,
+            error: action.error,
+         };
+
+      case LOGIN:
+         return {
+            ...state,
+            currentUser: action.payload,
+            loading: false,
+            message: null,
+         };
+
+      case REGISTER:
+         return {
+            ...state,
+            currentUser: action.payload,
+            loading: false,
+            message: null,
+         };
+
+      case LOGOUT:
+         return {
+            currentUser: null,
+            loading: false,
+            message: null,
+            error: null,
+         };
+
+      default:
+         return state;
+   }
+};
 
 export default authReducer;
