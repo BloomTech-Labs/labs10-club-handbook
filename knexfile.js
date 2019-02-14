51949547445e3ff59bc249d5dd16ab12ab518d47
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-14 17:53:29 -0500

    updated migrations/seeds for postgresql

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,35 @@
 
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./data/devDB.db3"
+      filename: './data/devDB.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
-    }
+      directory: './data/seeds',
+    },
   },
 
+  test: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'pass',
+      database: 'pg-testDB',
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    // useNullAsDefault: true,
+  },
   // development: {
   //   client: 'pg',
   //   connection: {
@@ -33,42 +49,42 @@ module.exports = {
   // },
 
   staging: {
-    client: "postgresql1",
+    client: 'postgresql1',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
-      directory: "./data/migrations"
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
-    }
+      directory: './data/seeds',
+    },
   },
 
   production: {
-    client: "postgresql1",
+    client: 'postgresql1',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
-      directory: "./data/migrations"
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
-    }
-  }
-};
+      directory: './data/seeds',
+    },
+  },
+}
