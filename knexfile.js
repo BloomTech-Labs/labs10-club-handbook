c1e96846f3a884be19692a92038d5406a5b215ab
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-14 18:08:25 -0500

    updated knexfile for postgress on heroku

--- knexfile.js
+++ knexfile.js
@@ -29,52 +29,11 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-    // useNullAsDefault: true,
-  },
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     host : '127.0.0.1',
-  //     user : '[db_username]',
-  //     password : '[db_password]',
-  //     database : '[db_name]',
-  //     charset: 'utf8'
-  //   },
-  //   migrations: {
-  //     directory: __dirname + '/knex/migrations',
-  //   },
-  //   seeds: {
-  //     directory: __dirname + '/knex/seeds'
-  //   }
-  // },
-
-  staging: {
-    client: 'postgresql1',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
   },
 
   production: {
-    client: 'postgresql1',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
