883227a200207efb76707a6f46250b8770438d51
Arthur Bates <aabatesj@gmail.com>
2019-02-14 20:41:10 -0500

    Fetch from backend
    fetch address to post charge token

--- client/src/components/billing/Payment.js
+++ client/src/components/billing/Payment.js
@@ -12,7 +12,7 @@ const PaymentContainer = styled.div`
 class Payment extends React.Component {
   onToken = (token) => {
     console.log(token);
-    fetch('/billing', {
+    fetch('http://localhost:5000/payment', {
       method: 'POST',
       body: JSON.stringify(token),
     }).then(response => {
