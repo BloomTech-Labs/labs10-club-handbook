896b015001bceb6739faee3d00b8d18a03887364
ToMMaaY <thuynh629@gmail.com>
2019-02-18 10:02:57 -0800

    initialized the clubs actions and reducer

---
+++ client/src/store/actions/clubActions.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const START = 'START';
+export const FAIL = 'FAIL';
+export const GET_CLUBS = 'GET_CLUBS';
+export const GET_CLUB_BY_ID = 'GET_CLUB_BY_ID';
+export const UPDATE_CLUB = 'UPDATE_CLUB';
+export const DELETE_CLUB = 'DELETE_CLUB';
+export const GET_CLUB_SECTIONS = 'GET_CLUB_SECTIONS';
+export const ADD_SECTION_BY_CLUB_ID = 'ADD_SECTION_BY_CLUB_ID';
+export const UPDATE_SECTION_BY_CLUB_ID = 'UPDATE_SECTION_BY_CLUB_ID';
+export const DELETE_ALL_SECTIONS_BY_CLUB_ID = 'DELETE_ALL_SECTIONS_BY_CLUB_ID';
+export const DELETE_ALL_SECTIONS_BY_CLUB_ID = 'DELETE_ALL_SECTIONS_BY_CLUB_ID';
+export const DELETE_SECTION_BY_SECTION_ID = 'DELETE_SECTION_BY_SECTION_ID';
+
+export const getClubs = () => dispatch => {
+
+}
