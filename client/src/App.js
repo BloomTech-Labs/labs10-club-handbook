ad529dbe32acbf2d1c296c1059c2c6af213aaadf
Arthur Bates <aabatesj@gmail.com>
2019-02-20 05:36:19 -0500

    Dashboard
    Beginnings of client dashboard
    - client sidenav
    - moved links to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Dashboard from './components/Dashboard'
 import Payment from './components/billing/Payment'
 import Members from './components/users/Members'
 import Login from './auth/Login'
@@ -10,6 +9,7 @@ import Authenticated from './auth/Authenticated'
 import SectionForm from './components/sections/SectionForm'
 import Settings from './components/Settings'
 import Landing from './components/Landing'
+import DashDrawer from './components/Dashboard'
 
 class App extends Component {
   render() {
@@ -18,6 +18,7 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route exact path="/" component={Landing} />
+            <Route exact path="/Dashboard" component={DashDrawer} />
             <Route exact path="/members" component={Members} />
             <Route exact path="/billing" component={Payment} />
             <Route exact path="/settings" component={Settings} />
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import Paper from '@material-ui/core/Paper';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react'
+import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import Paper from '@material-ui/core/Paper'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
-
-
 
 const styles = theme => ({
   appBar: {
@@ -60,87 +58,108 @@ const styles = theme => ({
   mainGrid: {
     marginTop: theme.spacing.unit * 3,
   },
-  
-});
+})
 
 const subscriptions = [
   {
     title: 'Startup',
     price: '0',
-    description: [
-        '5 members', 
-      ],
+    description: ['5 members'],
     buttonText: 'Sign up',
     buttonVariant: 'outlined',
   },
   {
     title: 'Small Biz',
     price: '5',
-    description: [
-      '20 members',
-    ],
+    description: ['20 members'],
     buttonText: 'Great value',
     buttonVariant: 'outlined',
   },
   {
     title: 'Enterprise',
     price: '20',
-    description: [
-      '500 members',
-    ],
+    description: ['500 members'],
     buttonText: 'Get started',
     buttonVariant: 'outlined',
   },
-];
+]
 
-function Landing(props)  {
-  const { classes } = props;
+function Landing(props) {
+  const { classes } = props
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static" color="default" className={classes.appBar}>
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
             Club.Handbook
           </Typography>
-          <Button component={Link} to="/members">Members</Button>
-          <Button component={Link} to="/section-form">SectionForm</Button>
-          <Button component={Link} to="/settings">Settings</Button>
-          <Button component={Link} to="/login" color="primary" variant="outlined">
+          <Button component={Link} to="/dashboard" color="inherit">
+            Dashboard
+          </Button>
+          <Button
+            component={Link}
+            to="/login"
+            color="inherit"
+            variant="outlined"
+          >
             Login
           </Button>
         </Toolbar>
       </AppBar>
       <main className={classes.layout}>
-      <Paper className={classes.mainLandingPost}>
-            <Grid container>
-              <Grid item md={6}>
-                <div className={classes.mainLandingPostContent}>
-                  <Typography variant="h3" color="TextPrimary" gutterBottom>
+        <Paper className={classes.mainLandingPost}>
+          <Grid container>
+            <Grid item md={6}>
+              <div className={classes.mainLandingPostContent}>
+                <Typography variant="h3" color="TextPrimary" gutterBottom>
                   To happier, productive, and loyal employees.
-                  </Typography>
-                  <Typography variant="h6" color="TextPrimary" paragraph>
+                </Typography>
+                <Typography variant="h6" color="TextPrimary" paragraph>
                   Top companies choose Club.Handbook to build their employee
-                   handbooks. Complete templates, beautiful design and easy customization.
-                    Publish as a mobile-friendly digital handbook.
-                  </Typography>
-                </div>
-              </Grid>
+                  handbooks. Complete templates, beautiful design and easy
+                  customization. Publish as a mobile-friendly digital handbook.
+                </Typography>
+              </div>
             </Grid>
-          </Paper>
+          </Grid>
+        </Paper>
         <div className={classes.landingContent}>
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
             Packages
           </Typography>
-          <Typography variant="h6" align="center" color="textSecondary" component="p">
-          Optimized for desktop, tablet, and mobile viewing. Update in real-time. Password
-           protect your handbook. Custom domains (ie. handbook.yourcompany.com)
+          <Typography
+            variant="h6"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Optimized for desktop, tablet, and mobile viewing. Update in
+            real-time. Password protect your handbook. Custom domains (ie.
+            handbook.yourcompany.com)
           </Typography>
         </div>
         <Grid container spacing={40} alignItems="flex-end">
           {subscriptions.map(tier => (
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === 'Enterprise' ? 12 : 6}
+              md={4}
+            >
               <Card>
                 <CardHeader
                   title={tier.title}
@@ -165,7 +184,13 @@ function Landing(props)  {
                   ))}
                 </CardContent>
                 <CardActions className={classes.cardActions}>
-                  <Button component={Link} to="/login" fullWidth variant={tier.buttonVariant} color="primary">
+                  <Button
+                    component={Link}
+                    to="/login"
+                    fullWidth
+                    variant={tier.buttonVariant}
+                    color="primary"
+                  >
                     {tier.buttonText}
                   </Button>
                 </CardActions>
@@ -173,24 +198,46 @@ function Landing(props)  {
             </Grid>
           ))}
           <div className={classes.landingContent}>
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               All in one easy-to-use platform.
             </Typography>
-            <Typography variant="h6" align="center" color="textSecondary" paragraph>
-            Optionally add a password to your handbook and change it anytime so you are
-             always in control of who can access it. Require employees to sign your handbook
-              with a legally binding electronic signature. Invite as many people as you'd
-               like to help with the handbook. They'll be able to make changes anytime.
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Optionally add a password to your handbook and change it anytime
+              so you are always in control of who can access it. Require
+              employees to sign your handbook with a legally binding electronic
+              signature. Invite as many people as you'd like to help with the
+              handbook. They'll be able to make changes anytime.
             </Typography>
             <div>
               <Grid container spacing={16} justify="center">
                 <Grid item>
-                  <Button component={Link} to="/login" variant="outlined" color="primary">
+                  <Button
+                    component={Link}
+                    to="/login"
+                    variant="outlined"
+                    color="primary"
+                  >
                     Sign Up
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button component={Link} to="/login" variant="contained" color="primary">
+                  <Button
+                    component={Link}
+                    to="/login"
+                    variant="contained"
+                    color="primary"
+                  >
                     Contact Us
                   </Button>
                 </Grid>
@@ -200,11 +247,11 @@ function Landing(props)  {
         </Grid>
       </main>
     </React.Fragment>
-  );
+  )
 }
 
 Landing.propTypes = {
   classes: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(Landing);
+export default withStyles(styles)(Landing)
