41a32b1c343a7f18de77303b4c03fa014f9b27c9
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-19 20:13:56 -0500

    fixed uncommented syntax in auth

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -8,14 +8,8 @@ class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'club-handbook.auth0.com',
     clientID: 'LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU',
-<<<<<<< HEAD
     redirectUri: 'https://clubhandbook.netlify.com/callback',
     // redirectUri: 'http://localhost:3000/callback',
-    // redirectUri: process.env.ENV_REDIRECT_URI,
-=======
-    redirectUri: "https://clubhandbook.netlify.com/callback",
-    // redirectUri: 'http://localhost:3000/callback',
->>>>>>> b2f2292cafe8d7d892ac135d4f4267a7732ab783
     audience: 'https://club-handbook.herokuapp.com/',
     responseType: 'token id_token',
     scope: 'openid profile email',
@@ -29,15 +23,6 @@ class Auth {
     return new Promise((resolve, reject) => {
       this.auth0.parseHash((err, authResults) => {
         if (authResults && authResults.accessToken && authResults.idToken) {
-<<<<<<< HEAD
-          let expiresAt = JSON.stringify(
-            authResults.expiresIn * 1000 + new Date().getTime()
-          )
-          localStorage.setItem('access_token', authResults.accessToken)
-          localStorage.setItem('id_token', authResults.idToken)
-          localStorage.setItem('expires_at', expiresAt)
-          console.log(authResults)
-=======
           console.log('handleAuthentication() invoked from Auth.js')
 
           let expiresAt = JSON.stringify(
@@ -50,7 +35,6 @@ class Auth {
 
           console.log(authResults)
 
->>>>>>> b2f2292cafe8d7d892ac135d4f4267a7732ab783
           resolve(authResults)
         } else if (err) {
           reject(err)
@@ -77,31 +61,16 @@ class Auth {
     } else {
       return {}
     }
-<<<<<<< HEAD
-  }
-
-  sendEmail = emailAddress => {
-    this.auth0.passwordlessStart(
-      { connection: 'email', send: 'link', email: emailAddress },
-      (err, res) => {
-        console.log(err)
-      }
-    )
-=======
->>>>>>> b2f2292cafe8d7d892ac135d4f4267a7732ab783
   }
 }
 
-<<<<<<< HEAD
-=======
-  // sendEmail = emailAddress => {
-  //   console.log('sendEmail from Auth.js invoked');
+// sendEmail = emailAddress => {
+//   console.log('sendEmail from Auth.js invoked');
 
-  //   this.auth0.passwordlessStart({ connection: 'email', send: 'link', email: emailAddress }, (err, res) => {
-  //     console.log(err);
-  //   })
-  // }
-}
+//   this.auth0.passwordlessStart({ connection: 'email', send: 'link', email: emailAddress }, (err, res) => {
+//     console.log(err);
+//   })
+// }
+// }
 
->>>>>>> b2f2292cafe8d7d892ac135d4f4267a7732ab783
 export default Auth
