039544f25d1c4f2edf912ab13313b0347fddd1f3
turkworks <andrewcengiz@gmail.com>
2019-02-21 11:24:08 -0700

    signature component created

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,10 @@ import SectionForm from './components/sections/SectionForm'
 import Settings from './components/Settings'
 import Landing from './components/Landing'
 import DashDrawer from './components/Dashboard'
-import CallbackEmail from './auth/CallbackEmail';
+import CallbackEmail from './auth/CallbackEmail'
 import AddMembers from './components/users/AddMembers'
 import UpdateMembers from './components/users/UpdateMembers'
+import Signature from './components/signature/Signature'
 
 class App extends Component {
   render() {
@@ -24,6 +25,7 @@ class App extends Component {
             <Route exact path="/Dashboard" component={DashDrawer} />
             <Route exact path="/members" component={Members} />
             <Route exact path="/add-members" component={AddMembers} />
+            <Route exact path="/signature" component={Signature} />
             <Route exact path="/update-members/:id" component={UpdateMembers} />
             <Route exact path="/billing" component={Payment} />
             <Route exact path="/settings" component={Settings} />
