39decc3d121f9cc7426d3b46957fe7f4a6dcf025
Arthur Bates <aabatesj@gmail.com>
2019-02-25 16:59:14 -0500

    Nav changes
    nav added to handbookMemberView and Members

--- client/src/components/handbook/HandbookMemberView.js
+++ client/src/components/handbook/HandbookMemberView.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getClubById } from '../../store/actions/clubActions';
+import { getClubById } from '../../store/actions/clubActions'
 import DashDrawer from '../Dashboard'
 
 class HandbookMemberView extends React.Component {
+  componentDidMount() {
+    const clubId = this.props.currentUser.club_id
 
-    componentDidMount() {
-        const clubId = this.props.currentUser.club_id
+    this.props.getClubById(clubId)
+  }
 
-        this.props.getClubById(clubId)
-
-    };
-
-
-    render() {
-        return (
-           <div>
-            <DashDrawer />
-              <div>hi
-                {/* if (server is sending the client a handbook, that means this user is authenticated and we know who they are and what group they belong to ) {
+  render() {
+    return (
+      <div>
+        <DashDrawer />
+        <div>
+          hi
+          {/* if (server is sending the client a handbook, that means this user is authenticated and we know who they are and what group they belong to ) {
                     display the handbook using the html render component
                 } else {
                     display <MagicLinkRequest />
                 } */}
-              </div>
-           </div>
+        </div>
+      </div>
     )
   }
 }
@@ -34,8 +32,9 @@ const mapStateToProps = state => {
     currentUser: state.auth.currentUser,
     club: state.clubs.clubById,
   }
-  
-  export default connect(
-    mapStateToProps,
-    { getClubById }
-  )(HandbookMemberView)
+}
+
+export default connect(
+  mapStateToProps,
+  { getClubById }
+)(HandbookMemberView)
