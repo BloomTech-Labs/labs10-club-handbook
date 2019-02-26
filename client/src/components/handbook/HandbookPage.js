03a1c4b88acb111ab99bb5b452db882e2e858490
ToMMaaY <thuynh629@gmail.com>
2019-02-26 14:09:40 -0800

    changed some layout styling

--- client/src/components/handbook/HandbookPage.js
+++ client/src/components/handbook/HandbookPage.js
@@ -92,9 +92,10 @@ class HandbookPage extends React.Component {
 
   render() {
     return (
-      <HandbookPageContainer>
+      <>
         <DashDrawer />
-        {/* <h1>Handbook Page!</h1>
+        <HandbookPageContainer>
+          {/* <h1>Handbook Page!</h1>
         {this.state.hasClub ? (
           <div>
             <h2>You have a Club!</h2>
@@ -104,95 +105,99 @@ class HandbookPage extends React.Component {
           <h2>You have NO club...</h2>
         )} */}
 
-        <HandbookForm
-          onSubmit={this.state.hasClub ? this.updateClub : this.createClub}
-        >
-          <div className="buttons">
-            <button type="button" onClick={this.sectionViewOn}>
-              Sections
-            </button>
-            <button type="button" onClick={this.sectionViewOff}>
-              Details
-            </button>
-          </div>
-          {this.state.sectionView ? (
-            //section view display
-            <div className="section-block">
-              <h1>Sections:</h1>
-              {this.props.sections.map(section => (
-                <>
-                  <div
-                    key={section.id}
-                    onClick={() => this.toggleEditView(section.id)}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    <img src={section.img_url} />
-                    <h2>{section.title}</h2>
-                  </div>
-                </>
-              ))}
-              <button onClick={this.toggleAddView}>Add Section</button>
+          <HandbookForm
+            onSubmit={this.state.hasClub ? this.updateClub : this.createClub}
+          >
+            <div className="buttons">
+              <button type="button" onClick={this.sectionViewOn}>
+                Sections
+              </button>
+              <button type="button" onClick={this.sectionViewOff}>
+                Details
+              </button>
             </div>
-          ) : (
-            //details view display
-            <div className="title-input">
-              <h2>Handbook Title:</h2>
-              <input
-                type="text"
-                name="title"
-                value={this.state.title}
-                onChange={this.handleChange}
+            {this.state.sectionView ? (
+              //section view display
+              <div className="section-block">
+                <h1>Sections:</h1>
+                {this.props.sections.map(section => (
+                  <>
+                    <div
+                      key={section.id}
+                      onClick={() => this.toggleEditView(section.id)}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      <img src={section.img_url} />
+                      <h2>{section.title}</h2>
+                    </div>
+                  </>
+                ))}
+                <button onClick={this.toggleAddView}>Add Section</button>
+              </div>
+            ) : (
+              //details view display
+              <div className="title-input">
+                <h2>Handbook Title:</h2>
+                <input
+                  type="text"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+                {this.state.hasClub ? (
+                  <button type="submit">Update Handbook</button>
+                ) : (
+                  <button type="submit">Create Handbook</button>
+                )}
+              </div>
+            )}
+          </HandbookForm>
+
+          {this.state.editView ? (
+            <SectionEditor>
+              <SectionForm
+                cancel={this.toggleEditView}
+                update
+                sectionId={this.state.sectionId}
               />
-              {this.state.hasClub ? (
-                <button type="submit">Update Handbook</button>
-              ) : (
-                <button type="submit">Create Handbook</button>
-              )}
-            </div>
-          )}
-        </HandbookForm>
+            </SectionEditor>
+          ) : null}
 
-        {this.state.editView ? (
-          <SectionEditor>
-            <SectionForm
-              cancel={this.toggleEditView}
-              update
-              sectionId={this.state.sectionId}
-            />
-          </SectionEditor>
-        ) : null}
+          {this.state.addView ? (
+            <SectionEditor>
+              <SectionForm cancel={this.cancel} />
+            </SectionEditor>
+          ) : null}
 
-        {this.state.addView ? (
-          <SectionEditor>
-            <SectionForm cancel={this.cancel} />
-          </SectionEditor>
-        ) : null}
-
-        <HandbookPreview>
-          <h1>{this.props.club.name}</h1>
-          {this.props.sections.map(section => (
-            <div>
-              <h2>{section.title}</h2>
-              <p>{section.body}</p>
-            </div>
-          ))}
-        </HandbookPreview>
-      </HandbookPageContainer>
+          <HandbookPreview>
+            <h1>{this.props.club.name}</h1>
+            {this.props.sections.map(section => (
+              <div>
+                <h2>{section.title}</h2>
+                <p>{section.body}</p>
+              </div>
+            ))}
+          </HandbookPreview>
+        </HandbookPageContainer>
+      </>
     )
   }
 }
 const HandbookPageContainer = styled.div`
   display: flex;
-  align-items: flex-start;
-  margin-top: 100px;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 120rem;
+  margin: 0 auto;
+  margin-top: 5rem;
+  border: 1px solid black;
 `
 
 const HandbookForm = styled.form`
-  margin-left: 170px;
-  width: 300px;
-  min-height: 400px;
-  padding: 20px;
-  border: 1px solid black;
+  max-width: 25%;
+  min-height: 40rem;
+  padding: 2rem;
+  border: 1px solid blue;
   .section-block {
     img {
       min-height: 30px;
@@ -213,10 +218,10 @@ const SectionEditor = styled.div`
 `
 
 const HandbookPreview = styled.div`
-  width: 500px;
-  min-height: 400px;
-  padding: 20px;
-  border: 1px solid black;
+  max-width: 70%;
+  min-height: 40rem;
+  padding: 2rem;
+  border: 1px solid red;
 `
 const mapStateToProps = state => {
   return {
