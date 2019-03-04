73412c66a32864543319fe34328cbcbfad550fca
Arthur Bates <aabatesj@gmail.com>
2019-03-04 16:23:43 -0500

    Theme
    material colors can be imported
    landing photo WIP

---
+++ client/src/style/colors.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { orange, blue } from '@material-ui/core/colors'
+
+export default createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: blue,
+  },
+})
