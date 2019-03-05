0863ca347810f7df3dc6ee0c061b86e58da421fa
ToMMaaY <thuynh629@gmail.com>
2019-03-05 12:06:05 -0800

    restyled the handbook/section side bar and made it fixed left

--- client/src/components/handbook/HandbookForm.js
+++ client/src/components/handbook/HandbookForm.js
@@ -5,50 +5,70 @@ import DetailsView from './views/DetailsView'
 import { connect } from 'react-redux'
 import { deleteSectionById } from '../../store/actions/clubActions'
 
-import { Paper, Tabs, Tab } from '@material-ui/core'
+// import { Paper, Tabs, Tab } from '@material-ui/core'
 import { size } from '../../style/breakpoints'
 
 const ClubForm = styled.form`
-  width: 25%;
-  min-height: 40rem;
-  border: 1px solid gray;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
+  position: fixed;
+  width: 20rem;
+  height: 100%;
+  z-index: 1;
+  left: 0;
+  background-color: rgb(60, 63, 67);
+  color: white;
+  overflow-x: hidden;
+  overflow: scroll;
+  border-left: 1px solid gray;
   box-shadow: 0 1px 1px gray;
+`
 
-  @media ${size.laptop} {
-    width: 29%;
-  }
+const Tabs = styled.div`
+  display: flex;
+  height: 5rem;
+  background: rgb(76, 81, 91);
+`
 
-  @media ${size.tablet} {
-    width: 60%;
-    margin: 0 auto;
-  }
-
-  @media ${size.mobile} {
-    width: 90%;
-  }
+const Tab = styled.div`
+  width: 50%;
+  color: ${props => (props.selected ? 'white' : 'whitesmoke')};
+  border-bottom: ${props => (props.selected ? '2px solid red' : 'none')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `
 
 const HandbookForm = props => {
   return (
     <>
       <ClubForm onSubmit={props.hasClub ? props.updateClub : props.createClub}>
-        <Paper square>
-          <Tabs
-            value={props.value}
-            indicatorColor="primary"
-            textColor="primary"
-            onChange={props.handleTabChange}
-          >
-            <Tab
-              label="Overview"
-              onClick={props.sectionViewOff}
-              fontSize="large"
-            />
-            <Tab label="Sections" onClick={props.sectionViewOn} />
-          </Tabs>
-        </Paper>
+        {/* <Paper square> */}
+        <Tabs
+          value={props.value}
+          indicatorColor="primary"
+          textColor="inherit"
+          variant="fullWidth"
+          onChange={props.handleTabChange}
+        >
+          {props.sectionView ? (
+            <>
+              <Tab onClick={props.sectionViewOff}>Overview</Tab>
+              <Tab selected label="Sections" onClick={props.sectionViewOn}>
+                Sections
+              </Tab>
+            </>
+          ) : (
+            <>
+              <Tab selected onClick={props.sectionViewOff}>
+                Overview
+              </Tab>
+              <Tab label="Sections" onClick={props.sectionViewOn}>
+                Sections
+              </Tab>
+            </>
+          )}
+        </Tabs>
+        {/* </Paper> */}
 
         <div style={{ padding: '2rem' }}>
           {props.sectionView ? (
