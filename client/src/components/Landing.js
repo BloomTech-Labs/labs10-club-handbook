d501bfebfe0902cde6e549077f83d96c307c7173
Arthur Bates <aabatesj@gmail.com>
2019-03-05 16:43:10 -0500

    Link fixes
    Landing buttons link to Auth0 login

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -267,8 +267,8 @@ class Landing extends React.Component {
                       </CardContent>
                       <CardActions className={classes.cardActions}>
                         <Button
-                          component={Link}
-                          to="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
+                          onClick={Link}
+                          href="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
                           fullWidth
                           variant={tier.buttonVariant}
                           color="primary"
@@ -308,8 +308,8 @@ class Landing extends React.Component {
             <Grid container spacing={16} justify="center">
               <Grid item>
                 <Button
-                  component={Link}
-                  to="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
+                  onClick={Link}
+                  href="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
                   variant="outlined"
                   color="primary"
                 >
@@ -318,8 +318,8 @@ class Landing extends React.Component {
               </Grid>
               <Grid item>
                 <Button
-                  component={Link}
-                  to="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
+                  onClick={Link}
+                  href="https://club-handbook.auth0.com/login?state=g6Fo2SBnaG93SXFoSmd2ZURjMVJ5ZGtfajFxcnVVMVhYcWNEcaN0aWTZIDg4Y3h5Z29qVzFNaUFoTHNPcGE4dEJHcjZxX0s5eXM4o2NpZNkgTEw1V0wzWUQ3dnhPWjV0dzV5TURtdFFiMlF4UnBUa1U&client=LL5WL3YD7vxOZ5tw5yMDmtQb2QxRpTkU&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Fclubhandbook.netlify.com%2Fcallback&scope=openid%20profile%20email&audience=https%3A%2F%2Fclub-handbook.herokuapp.com%2F&nonce=HyZ9_DQHaKIlzNyw-jglUXZILckQGMHR&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMC4wIn0%3D"
                   variant="contained"
                   color="primary"
                 >
