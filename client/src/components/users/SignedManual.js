332bbc4a8de9ba52d1ac7cb858f90ce1e9aa8b22
darenlarson <larson.daren@gmail.com>
2019-03-07 12:34:58 -0600

    switched back signed manual to class component for now bc I'm not able to test changes

--- client/src/components/users/SignedManual.js
+++ client/src/components/users/SignedManual.js
@@ -4,31 +4,31 @@ import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank'
 import Checkbox from '@material-ui/icons/CheckBox'
 // import Copyright from 'material-ui/icons/Copyright'
 
-const SignedManual = props => {
-  if (props.signed === true) {
-    return <Checkbox />
-  } else {
-    return <CheckBoxOutlineBlank />
-  }
-}
-
-export default SignedManual
-
-// class SignedManual extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-
-//   render() {
-//     if (this.props.signed === true) {
-//       // console.log('signed props', this.props.signed)
-//       return <Checkbox />
-//     } else {
-//       return <CheckBoxOutlineBlank />
-//     }
+// const SignedManual = props => {
+//   if (props.signed === true) {
+//     return <Checkbox />
+//   } else {
+//     return <CheckBoxOutlineBlank />
 //   }
 // }
 
 // export default SignedManual
+
+class SignedManual extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if (this.props.signed === true) {
+      // console.log('signed props', this.props.signed)
+      return <Checkbox />
+    } else {
+      return <CheckBoxOutlineBlank />
+    }
+  }
+}
+
+export default SignedManual
 
 
