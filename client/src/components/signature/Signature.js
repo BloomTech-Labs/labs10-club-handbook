6490d5eaf3e75b667b9f1e2e25ec6911ab636671
darenlarson <larson.daren@gmail.com>
2019-03-09 21:33:06 -0600

    Made side bar in members view collapseable. Added button to nav bar to show side bar. Added button to nav bar to sign handbook (this button doesn't render if member already signed). Responsibleness done.

--- client/src/components/signature/Signature.js
+++ client/src/components/signature/Signature.js
@@ -6,17 +6,29 @@ import { memberSigned } from '../../store/actions/usersActions'
 import styled from 'styled-components';
 import { size } from '../../style/breakpoints'
 
-const SignatureContainer = styled.div`
-  width: 500px;
+const SContainer = styled.div`
   position: fixed;
-  left: 230px;
-  top: 75px;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  z-index: 20;
+`;
+
+const SignatureContainer = styled.div`
+  width: 400px;
+  margin-top: 100px;
   padding: 20px;
   border: 1px solid gray;
   border-radius: 5px;
   box-shadow: 0 1px 10px gray;
-  z-index: 10;
+  z-index: 20;
   background: white;
+  @media ${size.mobile} {
+    width: 95%;
+  }
 `
 
 const Form = styled.form`
@@ -71,16 +83,18 @@ class Signature extends React.Component {
 
   render() {
     return (
-      <SignatureContainer>
-          <p>I have read the document and agree to the terms as currently stated in the document and any changes in the future.</p>
-          
-          <Form onSubmit={this.handleSubmit}>
-              <input type="text" name="signature" onChange={this.handleChanges} placeholder="Enter your full name" value={this.state.signature} />
-              <Button type="submit">Sign</Button>
-            </Form>
+      <SContainer>
+        <SignatureContainer>
+            <p>I have read the document and agree to the terms as currently stated in the document and any changes in the future.</p>
+            
+            <Form onSubmit={this.handleSubmit}>
+                <input type="text" name="signature" onChange={this.handleChanges} placeholder="Enter your full name" value={this.state.signature} />
+                <Button type="submit">Sign</Button>
+              </Form>
 
-          <Notification>{this.state.signatureStatus === true && 'Signature submited!'}</Notification>
-      </SignatureContainer>
+            <Notification>{this.state.signatureStatus === true && 'Signature submited!'}</Notification>
+        </SignatureContainer>
+      </SContainer>
     )
   }
 }
